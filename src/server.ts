f7876162e34182e308167289b581ce349e56e3ec
Chris Lee <cl114@illinois.edu>
2022-09-26 16:42:10 -0500

    project init done

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,11 @@ app.get("/", async (request: Request, response: Response) => {
   response.send("Hello world");
 });
 
+// route error handling: route doesn't exist
+app.use((request: Request, response: Response, next) => {
+  response.status(404).send("404 Error, Endpoint Doesn't Exist");
+});
+
 app.listen(port, (): void => {
   console.log(`App is listening at ${port}`);
 });
