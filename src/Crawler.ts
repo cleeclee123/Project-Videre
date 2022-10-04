4827defb823fa3ec36c2e150d818d879bf45ce6d
Chris Lee <cl114@illinois.edu>
2022-10-04 17:39:16 -0500

    updated

---
+++ src/Crawler.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosResponse, AxiosError, AxiosHeaders } from "axios";
+import { load } from "cheerio";
+
+// interface IUrl {
+//   link: string;
+// }
+
+// type PageResponse = {
+//   data: IUrl[];
+// }
+
+export const crawl = async (url: string) => {
+  await axios.get(url).then(async (response) => {
+    // load function from cheerio
+    const $ = load(response.data);
+
+    // grabbing all links that cheerio sees
+    const links = $("a")
+      .map((i, e) => e.attribs.href)
+      .get();
+
+    console.log(links);
+  });
+};
+
+crawl("https://en.wikipedia.org/wiki/Orlando,_Florida");
