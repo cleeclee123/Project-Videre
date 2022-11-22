efb6a8cbe06e9a9b3c63d9672677ef9980adfcd4
Chris Lee <cl114@illinois.edu>
2022-11-22 16:36:22 -0600

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as proxy from "./Spoofer/proxy";
 
-proxy.rotateProxies(1).then(async (data) => {
-  console.log(data);
-});
+// proxy.rotateProxies(1).then(async (data) => {
+//   console.log(data);
+// });
 
 // proxy.writeFileProxies("../../data/sslproxies.txt", "https://www.sslproxies.org/");
 // proxy.writeFileProxies("../../data/usproxies.txt", "https://www.us-proxy.org/");
+
+// console.log(proxy.generateIP())
