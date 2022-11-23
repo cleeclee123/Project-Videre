3e767416eabd6af4ad3ca99e45673eaac4152242
Chris Lee <cl114@illinois.edu>
2022-11-23 17:56:33 -0600

    added first octet getter

--- src/Spoofer/ipaddress.ts
+++ src/Spoofer/ipaddress.ts
@@ -4,12 +4,29 @@ type GenIP = {
   class: string;
 };
 
+const getFirstOctet = (ip: GenIP): number => {
+  return Number(ip.ipaddress.substring(0, ip.ipaddress.indexOf(".")));
+}
+
+const ipClassSetter = (ip: GenIP) => {
+  let firstOctet: number = getFirstOctet(ip);
+  if (firstOctet >= 1 && firstOctet < 127) {
+    ip.class = "class a";
+  } else if (firstOctet >= 128 && firstOctet < 192) {
+    ip.class = "class b";
+  } else if (firstOctet >= 192 && firstOctet < 224) {
+    ip.class = "class c";
+  } else if (firstOctet >= 224 && firstOctet < 239) {
+    ip.class = "class d";
+  }
+}
+
 /**
  * This example returns a random number between the specified values. The returned value is no lower than (and may possibly equal) min, and is less than (and not equal) max.
  * @param min lower bound inclusive
  * @param max upper bound non-inclusive
  */
-const getRandomArbitrary = (min: number, max: number): number => {
+export const getRandomArbitrary = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
@@ -17,7 +34,7 @@ const getRandomArbitrary = (min: number, max: number): number => {
 export const generateClassA = (): GenIP => {
   // 127.xxx.xxx.xxx reserved for loopback and diagnostic functions
   let netID: number = getRandomArbitrary(1, 127);
-  let classAIP = `${netID}.${getRandomArbitrary(0, 256)}.${getRandomArbitrary(0, 256)}.${getRandomArbitrary(0, 256)}`;
+  let classAIP: string = `${netID}.${getRandomArbitrary(0, 256)}.${getRandomArbitrary(0, 256)}.${getRandomArbitrary(0, 256)}`;
 
   let classA = {} as GenIP;
   classA.netid = netID;
