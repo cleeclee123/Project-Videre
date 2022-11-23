b9509c2b6be9aaf6dbb29276f4f285bc46de6c99
Chris Lee <cl114@illinois.edu>
2022-11-23 17:55:46 -0600

    progress bu still slow

--- src/Spoofer/ping.ts
+++ src/Spoofer/ping.ts
@@ -1,4 +1,5 @@
-import { spawn } from "child_process";
+import { ChildProcessWithoutNullStreams, spawn } from "child_process";
+import process from "node:process";
 import * as util from "util";
 import { createRequire } from "module";
 
@@ -12,29 +13,128 @@ import { createRequire } from "module";
  *  - https://nodejs.org/docs/v8.1.4/api/child_process.html#child_process_child_process_spawn_command_args_options
  */
 
+type Packets = {
+  sent: number;
+  received: number;
+  lost: number;
+  rate: number;
+};
+
 const require = createRequire(import.meta.url);
 const exec = util.promisify(require("child_process").exec);
 
-const ping = async (host: any) => {
+export const ping = async (host: any) => {
   const { stdout, stderr } = await exec(`ping ${host}`);
   console.log(stdout);
   console.log(stderr);
 };
 
+export const pingChuck = (host: any, numberPacket: any) => {
+  const ping = spawn("ping", [host, "-n", numberPacket]);
 
-const pingChuck = (host: any) => {
-  const ping = spawn('ping', [host]);
-
-  ping.stdout.on('data', (data) => {
+  ping.stdout.on("data", (data) => {
     console.log(`stdout: ${data}`);
   });
-  
-  ping.stderr.on('data', (data) => {
+
+  ping.stderr.on("data", (data) => {
     console.log(`stderr: ${data}`);
   });
-  
-  ping.on('close', (code) => {
+
+  ping.on("close", (code) => {
     console.log(`child process exited with code ${code}`);
   });
-}
 
+  ping.on("SIGINT", () => {
+    console.log("here");
+  })
+};
+
+export const checkResponse = (host: any) => {
+  const kPackets: string = "2";
+  const ping: ChildProcessWithoutNullStreams = spawn(
+    "ping",
+    [host, "-n", kPackets],
+    { detached: true }
+  );
+
+  // stream standard output
+  ping.stdout.setEncoding("utf8");
+  ping.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+    if (String(data).toLowerCase().includes("request time out")) {
+      ping.stdout.pause();
+      ping.kill(1);
+    }
+    if (String(data).toLowerCase().includes("reply")) {
+      ping.stdout.pause();
+      ping.kill(0);
+    }
+  });
+  
+  // handling errors, just return false
+  ping.stderr.on("data", (data) => {
+    ping.kill(undefined);
+  });
+  
+  // handling interuptions
+  ping.on('SIGINT', () => {
+    console.log("here");
+  });
+
+
+  // handle stream close, return result/
+  // what i have so far, checks code of processed killed
+  // slow, still finishes request to the sever ie doesnt terminate process entirely
+  return new Promise((resolve, reject) => {
+    ping.on("exit", async (code) => {
+      if (code === 0) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    });
+    
+    // what i want to do
+    // // handle sucess
+    // ping.on("SIGINT", async () => {
+    //   resolve(true);
+    // });
+
+    // // handle failure
+    // ping.on("SIGQUIT", async () => {
+    //   resolve(false);
+    // });
+  });
+};
+
+/**
+ * builds packet object to be used in parsing all ip addresses
+ * sends 3 packets to server
+ * @param host, passed in ip address
+ */
+const getPackets = (host: any) => {
+  const kPackets: number = 3;
+  const ping: ChildProcessWithoutNullStreams = spawn("ping", [
+    `${host} -n ${kPackets}`,
+  ]);
+
+  // stream standard output
+  ping.stdout.on("data", (data) => {
+    if (String(data).toLowerCase().includes("request time out")) {
+      return false;
+    }
+    if (String(data).toLowerCase().includes("")) {
+    }
+  });
+
+  // handling errors, just return false
+  ping.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+    return false;
+  });
+
+  // handle stream close, nothing to do
+  ping.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
