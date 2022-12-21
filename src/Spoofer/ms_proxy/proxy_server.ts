26e283cba44678c517850047a7e6d5bf60fe8e74
Chris Lee <cl114@illinois.edu>
2022-12-21 15:17:43 -0600

    proxy server updates

--- src/Spoofer/ms_proxy/proxy_server.ts
+++ src/Spoofer/ms_proxy/proxy_server.ts
@@ -5,51 +5,69 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 export class Server {
   public app_: express.Application;
-  // private router_: express.Router;
-  private port_: number;
+  private router_: express.Router;
+  private localPort_: number;
+  // private host_: string;
+  // private proxyPort_: number;
 
-  constructor(port: number) {
+  constructor(localPort: number /* , host: string, proxyPort: number */) {
     this.app_ = express();
-    // this.router_ = express.Router();
-    this.port_ = port;
+    this.router_ = express.Router();
+    this.localPort_ = localPort;
+    // this.host_ = host;
+    // this.proxyPort_ = proxyPort;
     this.config();
     this.proxyRoutes();
   }
 
   private config(): void {
-    this.app_.set("port", this.port_);
+    this.app_.set("port", this.localPort_);
     this.app_.use(bodyParser.json());
     this.app_.use(bodyParser.urlencoded({ extended: false }));
+    this.app_.use(this.router_);
   }
 
   private proxyRoutes = (): void => {
-    // this.router_.use(cors());
-    // this.router_.use(express.json());
-
+    this.router_.use(cors());
     this.app_.use(cors());
+    this.router_.use(express.json());
     this.app_.use(express.json());
-
-    this.app_.get("/", async (request, response) => {
+    
+    this.app_.get("/", (request, response) => {
       // response.render('index', {});
       response.send("proxy service entry point");
     });
 
-    this.app_.use(
-      "placeholder",
+    // for the basic test
+    this.router_.use(
+      "/placeholder_basic",
       createProxyMiddleware({
         target: "https://httpbin.org/ip",
         changeOrigin: true,
         pathRewrite: {
-          [`^/placeholder`]: "",
+          [`^/placeholder_basic`]: "",
+        },
+      })
+    );
+
+    // for the google test
+    this.router_.use(
+      "/placeholder_google",
+      createProxyMiddleware({
+        target: "https://www.google.com/",
+        changeOrigin: true,
+        pathRewrite: {
+          [`^/placeholder_google`]: "",
         },
       })
     );
   };
 
   public start = (): void => {
-    this.app_.listen(this.port_),
-      () => {
-        console.log(`Server listening in port ${this.port_}`);
-      };
+    this.app_.listen(this.localPort_);
+    console.log(`Server listening in port ${this.localPort_}`);
   };
 }
+
+let server = new Server(3001);
+server.start();
