36eb3af66afdac0cc4676273ee7f538ea981b93b
Chris Lee <cl114@illinois.edu>
2022-12-22 21:29:42 -0600

    updated proxy server

--- src/Spoofer/ms_proxy/proxy_server.ts
+++ src/Spoofer/ms_proxy/proxy_server.ts
@@ -5,14 +5,14 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 export class Server {
   public app_: express.Application;
-  private router_: express.Router;
+  // private router_: express.Router;
   private localPort_: number;
   // private host_: string;
   // private proxyPort_: number;
 
   constructor(localPort: number /* , host: string, proxyPort: number */) {
     this.app_ = express();
-    this.router_ = express.Router();
+    // this.router_ = express.Router();
     this.localPort_ = localPort;
     // this.host_ = host;
     // this.proxyPort_ = proxyPort;
@@ -24,13 +24,12 @@ export class Server {
     this.app_.set("port", this.localPort_);
     this.app_.use(bodyParser.json());
     this.app_.use(bodyParser.urlencoded({ extended: false }));
-    this.app_.use(this.router_);
   }
 
   private proxyRoutes = (): void => {
-    this.router_.use(cors());
+    // this.router_.use(cors());
+    // this.router_.use(express.json());
     this.app_.use(cors());
-    this.router_.use(express.json());
     this.app_.use(express.json());
     
     this.app_.get("/", (request, response) => {
@@ -39,7 +38,7 @@ export class Server {
     });
 
     // for the basic test
-    this.router_.use(
+    this.app_.use(
       "/placeholder_basic",
       createProxyMiddleware({
         target: "https://httpbin.org/ip",
@@ -51,7 +50,7 @@ export class Server {
     );
 
     // for the google test
-    this.router_.use(
+    this.app_.use(
       "/placeholder_google",
       createProxyMiddleware({
         target: "https://www.google.com/",
