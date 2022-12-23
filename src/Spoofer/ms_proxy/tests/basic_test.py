619ea301dd5baa6bb4d8e9a1d1f080de5517279e
Chris Lee <cl114@illinois.edu>
2022-12-22 21:28:51 -0600

    scraper to get proxy examples and checks

--- src/Spoofer/ms_proxy/tests/basic_test.py
+++ src/Spoofer/ms_proxy/tests/basic_test.py
@@ -1,13 +1,17 @@
 import requests
 
-def extract(proxy):
+def check(proxy):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'}
     try:
         r = requests.get('https://httpbin.org/ip', headers=headers,
-                         proxies={'http': proxy, 'https': proxy}, timeout=2)
-        print(r.json(), ' | Works')
-    except:
-        print("error with proxy")
+                         proxies={'http': proxy, 'https': proxy}, timeout=5)
+        print(f'response: {r.json()}, {r.status_code}, proxy: {proxy}')
+        if (r.json()['origin'] == proxy[0:proxy.index(':')]):
+            return True
+        return False
+    except requests.ConnectionError as err:
+        print(f'error with {proxy}')
+        return False
 
-print(extract("https://113.57.178.157:443"))
+print(check("49.0.2.242:8090"))
