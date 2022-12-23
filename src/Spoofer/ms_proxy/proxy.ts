1de8a6c862a310e6d56daa4af586a0cd5e18c10d
Chris Lee <cl114@illinois.edu>
2022-12-22 23:36:44 -0600

    what i have rn idk what im doing

--- src/Spoofer/ms_proxy/proxy.ts
+++ src/Spoofer/ms_proxy/proxy.ts
@@ -1,23 +1,76 @@
 import * as net from "net";
+import fetch from "node-fetch";
+import HttpsProxyAgent from "https-proxy-agent";
 import * as ip from "./ipaddress.js";
 import { IPInfo } from "./ipaddress.js";
 import { Queue } from "../lib/queue.js";
+import { application } from "express";
 
-type ProxyInfo = {
+type ProxyParts = {
+  protocol: string;
+  host: string;
+  port: string;
+  proxyString: string;
+  httpbinTest: boolean;
+  googleTest: boolean;
+};
+
+type ProxiesInfo = {
   ipInfo: IPInfo;
-  proxies: Array<String>;
+  proxies: Array<ProxyParts>;
 };
 
 export class Proxy {
   private capacity_: number;
-  private queue_: Queue<ProxyInfo>;
+  private count_: number;
+  private queue_: Queue<ProxiesInfo>;
+  private currentHost_: string;
+  private currentPort_: string;
   private dateCreated_: Date;
+  private localAddress_: string;
 
   constructor(capacity: number) {
     this.capacity_ = capacity;
+    this.count_ = 0;
+    this.queue_ = new Queue<ProxiesInfo>(capacity);
+    this.currentHost_ = "";
+    this.currentPort_ = "";
     this.dateCreated_ = new Date();
-    this.queue_ = new Queue<ProxyInfo>(capacity);
+    this.localAddress_ = "";
   }
+
+  /**
+   * tests if proxies work by checking current ip address on httpbin
+   * @param proxy 
+   * @returns 
+   */
+  testHTTPBin = async (proxy: string) => {
+    let config = {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0",
+      },
+      agent: new HttpsProxyAgent.HttpsProxyAgent(proxy),
+    };
+    try {
+      return await fetch(`https://httpbin.org/ip`, config).then(
+        async (response) => {
+          let data: any = await response.json();
+          if (
+            proxy.substring(proxy.indexOf("/") + 2, proxy.indexOf(":", 6)) ===
+            data["origin"]
+          ) {
+            return true;
+          }
+          return false;
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  testGoogle = (proxy: string) => {};
 
   /**
    * attempts to connect to socket with passed in ip address on passed in port
@@ -53,126 +106,174 @@ export class Proxy {
    * @param port
    * @returns Promise<String> proxy string
    */
-  buildProxyHelper = async (host: string, port: string): Promise<String> => {
-    return await this.checkPort(host, port, 1000).then((data) => {
-      if (data) {
-        if (port === String(443)) {
-          return `https://${host}:${String(port)}`;
-        } else {
-          return `http://${host}:${String(port)}`;
+  buildProxyHelper = async (
+    host: string,
+    port: string
+  ): Promise<ProxyParts> => {
+    return await this.checkPort(host, port, 1000).then(async (data) => {
+      return new Promise(async (resolve, reject) => {
+        let current = {} as ProxyParts;
+        if (data) {
+          if (port === String(443)) {
+            current.host = host;
+            current.port = port;
+            current.protocol = "https";
+            current.proxyString = `https://${host}:${String(port)}`;
+            current.httpbinTest = false;
+            current.googleTest = false;
+            if (await this.testHTTPBin(`https://${host}:${String(port)}`)) {
+              current.httpbinTest = true;
+            }
+            // if (await this.testGoogle(`https://${host}:${String(port)}`)) {
+            //   current.googleTest = true;
+            // }
+            resolve(current);
+          } else {
+            current.host = host;
+            current.port = port;
+            current.protocol = "http";
+            current.proxyString = `http://${host}:${String(port)}`;
+            current.httpbinTest = false;
+            current.googleTest = false;
+            if (await this.testHTTPBin(`http://${host}:${String(port)}`)) {
+              current.httpbinTest = true;
+            }
+            // if (await this.testGoogle(`https://${host}:${String(port)}`)) {
+            //   current.googleTest = true;
+            // }
+            resolve(current);
+          }
         }
-      }
-      return "";
+        resolve(current);
+      })
     });
   };
 
   /**
-   * loops thru http(s) ports and checks if able to connect,
+   * loops thru http(s) ports and checks if able to connect, callback hell is inevitable
    * @param host
    * @returns Promise<Array<String>> array of proxies associated with the host
    */
-  buildProxy = async (host: string): Promise<Array<String>> => {
+  buildProxy = async (host: string): Promise<Promise<ProxyParts>[]> => {
     const kHttpPorts: Array<number> = [80, 8080, 8008, 443];
-    let resPromise = await Promise.all(
-      kHttpPorts.map((port) => {
-        return this.buildProxyHelper(host, String(port));
-      })
-    );
-    return resPromise.filter((e) => e !== "");
+    let res = kHttpPorts.map((port) => {
+      return this.buildProxyHelper(host, String(port))
+    });
+    return res;
   };
 
   /**
-   * temp function Class A
+   * generation for class a proxies
    */
   generateProxyClassA = async (): Promise<void> => {
-    let count = 0;
-    if (this.queue_.getCapacity() < this.queue_.size()) {
-      return;
-    }
-    while (count < this.capacity_) {
-      let proxyInfo = {} as ProxyInfo;
+    while (this.count_ < this.capacity_) {
+      let proxyInfo = {} as ProxiesInfo;
       proxyInfo.ipInfo = ip.generateClassA();
-      await this.buildProxy(proxyInfo.ipInfo.ipaddress).then(async (data) => {
-        if (data.length != 0) {
-          proxyInfo.proxies = data;
-          this.queue_.enqueue(proxyInfo);
-          this.printQueue();
-          count++;
+      await this.buildProxy(proxyInfo.ipInfo.ipaddress).then(
+        async (proxies) => {
+          let check: ProxyParts[] = [];
+          if (proxies.length != 0) {
+            let tempCheck: ProxyParts;
+            proxies.forEach((proxy) => {
+              proxy.then((p) => {
+                // tempCheck.
+                // tempCheck.httpbinTest = testHTTPBin(proxy);
+                // tempCheck.googleTest = testGoogle(proxy);
+                // check.push(tempCheck);
+              })
+            });
+            this.queue_.enqueue(proxyInfo);
+            this.count_++;
+          }
         }
-      });
-      console.log("scanning " + proxyInfo.ipInfo.ipaddress);
+      );
+      console.log(
+        `scanning ${proxyInfo.ipInfo.ipaddress} current count ${this.count_} CLASS A`
+      );
     }
-    console.log("class a scan done");
-  };
-  /**
-   * temp function Class B
-   */
-  generateProxyClassB = async (): Promise<void> => {
-    let count = 0;
-    if (this.queue_.getCapacity() < this.queue_.size()) {
-      return;
-    }
-    while (count < this.capacity_) {
-      let proxyInfo = {} as ProxyInfo;
-      proxyInfo.ipInfo = ip.generateClassB();
-      await this.buildProxy(proxyInfo.ipInfo.ipaddress).then(async (data) => {
-        if (data.length != 0) {
-          proxyInfo.proxies = data;
-          this.queue_.enqueue(proxyInfo);
-          this.printQueue();
-          count++;
-        }
-      });
-      console.log("scanning " + proxyInfo.ipInfo.ipaddress);
-    }
-    console.log("class b scan done");
-  };
-  /**
-   * temp function
-   */
-  generateProxyClassC = async (): Promise<void> => {
-    let count = 0;
-    if (this.queue_.getCapacity() < this.queue_.size()) {
-      return;
-    }
-    while (count < this.capacity_) {
-      let proxyInfo = {} as ProxyInfo;
-      proxyInfo.ipInfo = ip.generateClassC();
-      await this.buildProxy(proxyInfo.ipInfo.ipaddress).then(async (data) => {
-        if (data.length != 0) {
-          proxyInfo.proxies = data;
-          this.queue_.enqueue(proxyInfo);
-          this.printQueue();
-          count++;
-        }
-      });
-      console.log("scanning " + proxyInfo.ipInfo.ipaddress);
-    }
-    console.log("class c scan done");
+    console.log("CLASS A scan done");
   };
 
   /**
-   * temp function Class
+   * generation for class b proxies
    */
-  generateProxyClassD = async (): Promise<void> => {
-    let count = 0;
-    if (this.queue_.getCapacity() < this.queue_.size()) {
-      return;
+  // generateProxyClassB = async (): Promise<void> => {
+  //   while (this.count_ < this.capacity_) {
+  //     let proxyInfo = {} as ProxiesInfo;
+  //     proxyInfo.ipInfo = ip.generateClassB();
+  //     await this.buildProxy(proxyInfo.ipInfo.ipaddress).then(async (data) => {
+  //       if (data.length != 0) {
+  //         proxyInfo.proxies = data;
+  //         this.queue_.enqueue(proxyInfo);
+  //         this.count_++;
+  //         // console.log(`count updated ${this.count_}`);
+  //         if (this.count_ === this.capacity_) {
+  //           console.log("cap hit");
+  //         }
+  //       }
+  //     });
+  //     console.log(
+  //       `scanning ${proxyInfo.ipInfo.ipaddress} current count ${this.count_} CLASS B`
+  //     );
+  //   }
+  //   console.log("CLASS B scan done");
+  // };
+
+  /**
+   * generation for class c proxies
+   */
+  // generateProxyClassC = async (): Promise<void> => {
+  //   while (this.count_ < this.capacity_) {
+  //     let proxyInfo = {} as ProxyInfo;
+  //     proxyInfo.ipInfo = ip.generateClassC();
+  //     await this.buildProxy(proxyInfo.ipInfo.ipaddress).then(async (data) => {
+  //       if (data.length != 0) {
+  //         proxyInfo.proxies = data;
+  //         this.queue_.enqueue(proxyInfo);
+  //         this.count_++;
+  //         // console.log(`count updated ${this.count_}`);
+  //         if (this.count_ === this.capacity_) {
+  //           console.log("cap hit");
+  //         }
+  //       }
+  //     });
+  //     console.log(
+  //       `scanning ${proxyInfo.ipInfo.ipaddress} current count ${this.count_} CLASS C`
+  //     );
+  //   }
+  //   console.log("CLASS C scan done");
+  // };
+
+  /**
+   * generation for class d proxies
+   */
+  // generateProxyClassD = async (): Promise<void> => {
+  //   while (this.count_ < this.capacity_) {
+  //     let proxyInfo = {} as ProxyInfo;
+  //     proxyInfo.ipInfo = ip.generateClassD();
+  //     await this.buildProxy(proxyInfo.ipInfo.ipaddress).then(async (data) => {
+  //       if (data.length != 0) {
+  //         proxyInfo.proxies = data;
+  //         this.queue_.enqueue(proxyInfo);
+  //         this.count_++;
+  //         // console.log(`count updated ${this.count_}`);
+  //       }
+  //     });
+  //     // console.log("scanning " + proxyInfo.ipInfo.ipaddress);
+  //   }
+  //   // console.log("class d scan done");
+  // };
+
+  /**
+   * wrapper function for all proxy generation functions
+   * @param processes
+   */
+  generateProxyWrapper = async (processes: number): Promise<void | boolean> => {
+    for (let i = 0; i < processes; i++) {
+      // this.generateProxyClassA();
+      // this.generateProxyClassB();
+      // this.generateProxyClassC();
     }
-    while (count < this.capacity_) {
-      let proxyInfo = {} as ProxyInfo;
-      proxyInfo.ipInfo = ip.generateClassD();
-      await this.buildProxy(proxyInfo.ipInfo.ipaddress).then(async (data) => {
-        if (data.length != 0) {
-          proxyInfo.proxies = data;
-          this.queue_.enqueue(proxyInfo);
-          this.printQueue();
-          count++;
-        }
-      });
-      console.log("scanning " + proxyInfo.ipInfo.ipaddress);
-    }
-    console.log("class d scan done");
   };
 
   /**
@@ -180,16 +281,9 @@ export class Proxy {
    * @param processes
    */
   scanForProxies = async (processes: number) => {
-    this.generateProxyClassA();
-    this.generateProxyClassA();
-    // this.generateProxyClassB();
-    // this.generateProxyClassC();
-    //  for (let i = 0; i < processes; i++) {
-    //     this.generateProxyClassA();
-    //     this.generateProxyClassA();
-    //     this.generateProxyClassB();
-    //     this.generateProxyClassC();
-    //   }
+    return this.generateProxyWrapper(processes).then(async (data) => {
+      return data;
+    });
   };
 
   /**
@@ -199,5 +293,22 @@ export class Proxy {
     this.queue_.getStorage().forEach((e) => {
       console.log("ip info: " + e.ipInfo.ipaddress + " proxies: " + e.proxies);
     });
+    console.log(`proxies scanned on ${this.dateCreated_}`);
+  };
+
+  /**
+   *
+   * @returns
+   */
+  getCurrentHost = () => {
+    return this.currentHost_;
+  };
+
+  /**
+   *
+   * @returns
+   */
+  getCurrentPort = () => {
+    return this.currentPort_;
   };
 }
