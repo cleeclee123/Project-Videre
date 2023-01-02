ca10e7a03c37a4135d8758a4423b3df6a9ce553d
Chris Lee <cl114@illinois.edu>
2023-01-02 12:21:36 -0600

    doc

--- src/Spoofer/ms_proxy/types.ts
+++ src/Spoofer/ms_proxy/types.ts
@@ -92,6 +92,7 @@ export const uas: Array<string> = [
   "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
 ];
 
+// helper funtion to create request header and https prpxy agent for fetch
 export const fetchConfig = (host: string, port: string) => {
   return {
     headers: {
